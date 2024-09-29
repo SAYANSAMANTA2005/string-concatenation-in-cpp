6294229cda735cfa2c02dc4db983b49aa0cfe726
root <root@localhost>
2024-09-29 20:31:43 +0530

    auto publish 1727622103066

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+// C++ Program for string
+// concatenation using append
+#include <iostream>
+using namespace std;
+ 
+// Driver code
+int main()
+{
+    string init("sayan");
+    string add("samanta");
+ 
+    // Appending the string.
+    init.append(add);
+ 
+    cout << init << endl;
+    return 0;
+}
